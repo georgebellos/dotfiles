05e022e5934c66ff999a6ca6b20ca2575ce1d501
George Bellos <georgebellos@me.com>
2013-01-16 13:02:29 +0200

    show and hide files os x aliases

--- zsh/custom/osx.zsh
+++ zsh/custom/osx.zsh
@@ -3,3 +3,9 @@ alias switch='/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resou
 
 # battery remaining
 alias battery='pmset -g ps'
+
+# show hidden files
+alias sfiles='defaults write com.apple.Finder AppleShowAllFiles YES && killall Finder'
+
+# hide hidden files
+alias hfiles='defaults write com.apple.Finder AppleShowAllFiles NO && killall Finder'
