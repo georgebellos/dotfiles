301c3dc200df002f2bc05f42d95d4457a5b421b2
George Bellos <contact@georgebellos.com>
2013-11-24 00:08:23 +0200

    Update for postgres 9.2.4

--- postgres/start_postgres_server.sh
+++ postgres/start_postgres_server.sh
@@ -5,5 +5,5 @@ server_status=`/usr/local/bin/pg_ctl -D /usr/local/var/postgres status`
 if [ "$server_status" == "pg_ctl: no server running" ]; then
   postgres -D /usr/local/var/postgres
 else
-  tail -f /usr/local/var/postgres/server.log
+  tail -f /usr/local/var/postgres9.2.4/server.log
 fi
